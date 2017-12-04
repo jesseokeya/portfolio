4ee810ab3aa5160d61055e48c22b5b85f0b4c888
Jesse Okeya <Jesseokeya@gmail.com>
2017-12-04 12:11:23 -0500

    bug fix ✓

--- src/index.js
+++ src/index.js
@@ -4,5 +4,4 @@ import App from './components/App';
 import registerServiceWorker from './serviceWorkers/registerServiceWorker';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
-console.log(ReactDOM);
 registerServiceWorker();
