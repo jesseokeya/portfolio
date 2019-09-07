b8d12ca0ff2939bd1a0bb2d3d3e2d87c16dad416
jesseokeya <jesseokeya@gmail.com>
2019-09-07 02:35:52 -0400

    Update ProjectImage.js

--- src/components/common/ProjectImage.js
+++ src/components/common/ProjectImage.js
@@ -10,14 +10,36 @@ class ProjectImage extends Component {
   }
 
   render() {
+    const isMobile = window.innerWidth <= 800
     const data = this.state.data
-    return (<div className="col-sm-6 col-md-4 col-lg-3">
-      <div data-toggle="popover" data-trigger="hover" title={data.title} data-content={data.description}>
-        <ReactGA.OutboundLink eventLabel={data.title} to={data.url} target="_blank">
-          <img className="img-thumbnail rounded shadow" src={data.image} alt={data.title}/>
-        </ReactGA.OutboundLink>
+    const defaultImageStyles = { 
+      height: !isMobile ? '50vh' : '80vh', 
+      width: '100%',
+      textAlign: 'center'
+    }
+    return (
+      <div className="col-sm-6 col-md-4 col-lg-3">
+        <div
+          data-toggle="popover"
+          data-trigger="hover"
+          title={data.title}
+          data-content={data.description}
+        >
+          <ReactGA.OutboundLink
+            eventLabel={data.title}
+            to={data.url}
+            target="_blank"
+          >
+            <img
+              className="img-thumbnail rounded shadow"
+              src={data.image}
+              alt={data.title}
+              style={defaultImageStyles}
+            />
+          </ReactGA.OutboundLink>
+        </div>
       </div>
-    </div>);
+    );
   }
 }
 
